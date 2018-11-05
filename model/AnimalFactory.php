098c265519b46541b08aee50c5cd7d3b2054ace8
JustinasTa <tamosaitis@gmail.com>
2018-11-05 13:39:17 +0200

    First commit

---
+++ model/AnimalFactory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Justinas
+ * Date: 2018-11-05
+ * Time: 11:40
+ */
+
+class AnimalFactory
+{
+
+}
---
+++ model/Cat.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Justinas
+ * Date: 2018-11-05
+ * Time: 11:41
+ */
+
+include_once __DIR__."Animal.php";
+
+class Cat extends Animal
+{
+
+}
